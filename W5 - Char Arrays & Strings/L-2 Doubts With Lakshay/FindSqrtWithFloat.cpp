92aabd744e4c3854deda21c5bfe450cb22f8485b
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-06-07 23:02:12 +0530

    Daily backup - $ (7-June-2025)

---
+++ W5 - Char Arrays & Strings/L-2 Doubts With Lakshay/FindSqrtWithFloat.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std; 
+
+int FindSqrt(int num)
+{
+    int start = 0;
+    int end = num;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (mid * mid <= num)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+            end = mid - 1;
+        mid = start + (end - start) / 2;  
+    }
+    return ans;
+}
+
+double FindSqrtWithPoint(int num)
+{
+    double sqrt = FindSqrt(num);
+    double step = 0.1;
+    int precision = 10;
+    for (int i = 0; i < precision; i++)
+    {
+        double j = sqrt;
+        while (j * j <= num)
+        {
+            sqrt = j;
+            j += step;
+        }
+        step /= 10;
+    }
+    return sqrt;
+}
+int main() 
+{ 
+    int num;
+    cout << "Enter Number: "; 
+    cin >> num;
+
+    double ans = FindSqrtWithPoint(num);
+    cout << "Your Answer Is: " << ans << endl;
+    return 0; 
+}
