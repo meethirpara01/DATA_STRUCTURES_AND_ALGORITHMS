5e374cbc3d6dcd2622a8f869ce909b3ffd831966
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-08-30 21:37:14 +0530

    Daily backup - $(30-Aug-2025)

--- W8 - Backtracking & DnC/L-4 DnC Class_2/RatInaMaze.cpp
+++ W8 - Backtracking & DnC/L-4 DnC Class_2/RatInaMaze.cpp
@@ -9,12 +9,12 @@ using namespace std;
 bool isSafe(int maze[][4], int srcx, int srcy, int newx, int newy, int row, int col, vector<vector<bool>> &visited)
 {
     if ((newx >= 0 && newx < row) && (newy >= 0 && newy < col) && maze[newx][newy] == 1 && visited[newx][newy] == false)
-    {
-        
-    }
+        return true;
+    else
+        return false;
 }
 
-void PrintAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &output, vector<vector<bool>> &visited)
+void printAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &output, vector<vector<bool>> &visited)
 {
     // BASE CASE
     // DESTINATION COORDINATES ARE [ROW - 1], [COL - 1]
@@ -27,57 +27,84 @@ void PrintAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &o
 
 
     // SOLVE ONLY CASE ONE AND RECURSION WILL SEE
+    int newx = srcx - 1;
+    int newy = srcy;
 
     // UP
     // POSSIBLITYS
         // PATH CLOSED
         // IF OUT BOUND
         // CHECH IS POSITION IS ALREDY VISITED
-    if (isSafe())
+    if (isSafe(maze, srcx, srcy, newx, newy, row, col, visited))
     {
         // NOW MARK VISITED
+        visited[newx][newy] = true;
         // RECURSIVE CALL
+        output.push_back('U');
+        printAllPath(maze, row, col, newx, newy, output, visited);
         // BACKTRACKING
+        output.pop_back();
+        visited[newx][newy] = false;
     }
     
 
     // RIGHT
-    if (isSafe())
+    newx = srcx;
+    newy = srcy + 1;
+    if (isSafe(maze, srcx, srcy, newx, newy, row, col, visited))
     {
         // NOW MARK VISITED
+        visited[newx][newy] = true;
         // RECURSIVE CALL
+        output.push_back('R');
+        printAllPath(maze, row, col, newx, newy, output, visited);
         // BACKTRACKING
+        output.pop_back();
+        visited[newx][newy] = false;
     }
 
     // DOWN
-    if (isSafe())
+    newx = srcx + 1;
+    newy = srcy;
+    if (isSafe(maze, srcx, srcy, newx, newy, row, col, visited))
     {
         // NOW MARK VISITED
+        visited[newx][newy] = true;
         // RECURSIVE CALL
+        output.push_back('D');
+        printAllPath(maze, row, col, newx, newy, output, visited);
         // BACKTRACKING
+        output.pop_back();
+        visited[newx][newy] = false;
     }
 
     //LEFT
-    if (isSafe())
+    newx = srcx;
+    newy = srcy - 1;
+    if (isSafe(maze, srcx, srcy, newx, newy, row, col, visited))
     {
         // NOW MARK VISITED
+        visited[newx][newy] = true;
         // RECURSIVE CALL
+        output.push_back('L');
+        printAllPath(maze, row, col, newx, newy, output, visited);
         // BACKTRACKING
+        output.pop_back();
+        visited[newx][newy] = false;
     }
-    
-
 }
+
 int main()
 {
     int row;
     cout << "Enter Total Row Of Array: ";
     cin >> row;
     
-    int col;
-    cout << "Enter Total Col Of Array: ";
-    cin >> col;
+    int col = 4;
+    // cout << "Enter Total Col Of Array: ";
+    // cin >> col;
 
-    int maze[row][col];
+    int maze[row][4];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -104,7 +131,7 @@ int main()
     else
     {
         visited[srcx][srcy] = true;
-        PrintAllPath(maze, row, col, srcx, srcy, output, visited);
+        printAllPath(maze, row, col, srcx, srcy, output, visited);
     }
         
     return 0;
