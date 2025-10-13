dffbb7deb4a988ff4d43621c1b428d1e7983244c
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-10-13 19:13:50 +0530

    Daily backup - $(13-Oct-2025)

---
+++ W9 - OOPs Concepts/L-7 Can Copy Constructor Made Private?/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+    int width;
+    //ctor
+    Box(int _w) : width(_w) {};
+    
+    public:
+        
+
+        int getWidth() const
+        {
+            return width;
+        }
+
+        void setwidth(int _val)
+        {
+            width = _val;
+        }
+        friend class BoxFactory;
+};
+
+class BoxFactory
+{
+    int count;
+
+    public:
+        Box getABox(int _w)
+        {
+            ++count;
+            return Box(_w);
+        }
+};
+
+int main()
+{
+    BoxFactory bfact;
+    Box b =  bfact.getABox(5);
+    cout << b.getWidth() << endl;
+
+    // Box b(5);
+    // cout << b.getWidth() << endl;
+    return 0;
+}
