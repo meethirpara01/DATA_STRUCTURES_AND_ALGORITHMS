f49dac23b6e39226d8719041dd8fac8f1702d2a6
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-10-23 20:11:25 +0530

    Daily backup - $(23-Oct-2025)

--- W10 - Linked Lists/L-2 Linked Lists Class_2/DoublyLinkedList.cpp
+++ W10 - Linked Lists/L-2 Linked Lists Class_2/DoublyLinkedList.cpp
@@ -86,6 +86,122 @@ void insertAtTail(Node* &head, Node* &tail, int data)
     } 
 }
 
+void insertAtPosition(Node* &head, Node* &tail, int data, int position)
+{
+    int length = findLengthOfList(head);
+
+    // LIST IS EMPTY
+    if (head == NULL)
+    {
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else 
+    {
+        if (position <= 1)
+        {
+            insertAtHead(head, tail, data);
+        }
+        else if (position > length)
+        {
+            insertAtTail(head, tail, data);
+        }
+        else
+        {
+            // INSERT AT MIDDLE
+            // STEP1: CREAT AN NODE
+            Node* newNode = new Node(data);
+
+            // STEP2: SET PREV AND CURR POINTER
+            Node* prevNode = NULL;
+            Node* currNode = head;
+            while (position != 1)
+            {
+                prevNode = currNode;
+                currNode = currNode->next;
+                position--;
+            }
+
+            // STEP3: UPDATE POINTERS
+            prevNode->next = newNode;
+            newNode->prev = prevNode;
+            newNode->next = currNode;
+            currNode->prev = newNode;
+        } 
+    } 
+}
+
+void DeletionNode(Node *&head, Node *&tail, int position)
+{
+    int length = findLengthOfList(head);
+
+    // EMPTY LIST
+    if (head == NULL)
+    {
+        cout << "CAN NOT DELETE, BECAUSE LL IS EMPTY" << endl;
+        return;
+    }
+
+    // FOR SINGLE ELEMENT
+    else if (head == tail)
+    {
+        Node* temp = head;
+        delete temp;
+        head = NULL;
+        tail = NULL;
+    }
+
+    //DELETE FROM HEAD
+    else if (position <= 1)
+    {
+        // DELETE FIRST NODE
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        head->prev = NULL;
+        delete temp;
+    }
+
+    else if (position >= length)
+    {
+        // DELETE LAST NODE
+        Node* prevNode = tail->prev;
+
+        // POINT NULL TO PREVNODE AND TAIL
+        prevNode->next = NULL;
+        tail->prev = NULL;
+
+        // DELETE TAIL NODE 
+        delete tail;
+
+        // UPDATE TAIL
+        tail = prevNode;
+    }
+    else
+    {
+        // DELETE FROM MIDDLE
+        Node* prevNode = NULL;
+        Node* currNode = head;
+        while (position != 1)
+        {
+            prevNode = currNode;
+            currNode = currNode->next;
+            position--;
+        }
+        Node* nextNode = currNode->next;
+
+        // CONNECT PREVNODE AND NEXTNODE AND DELETE CURRNODE
+        prevNode->next = nextNode;
+        currNode->next = NULL;
+        currNode->prev = NULL;
+        nextNode->prev = prevNode;
+
+        // DELETE CURR
+        delete currNode;
+    }
+}
+
 int main()
 {
     Node* head = NULL;
@@ -98,6 +214,13 @@ int main()
     insertAtHead(head, tail, 20);
     insertAtHead(head, tail, 10);
     insertAtTail(head, tail, 50);
+    insertAtPosition(head, tail, 25, 3);
+    printList(head);
+    cout << endl;
+
+    DeletionNode(head, tail, 3);
+    DeletionNode(head, tail, 5);
+    DeletionNode(head, tail, 0);
     printList(head);
     cout << endl;
 
