3cdd5802771d4b1bf9a5fd13fccb5c593c9b13c5
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-10-27 14:23:00 +0530

    Daily backup - $(27-Oct-2025)

---
+++ W10 - Linked Lists/L-4 Linked Lists Class_4/Main.cpp
@@ -0,0 +1,17 @@
+// I LEARN IN THIS LECTURE
+    // SOLVE SIX QUESTIONS
+    // CHECK CYCLE IN LINKED LIST
+    // FIND STARTING POINT IN CYCLE 
+    // REMOVE CYCLE 
+    // ADD 1 TO LIKED LIST
+    // REVERSE LIST WITH K GROUPS
+    // REMOVE DUPLICATE IN SORTED LISTS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "LINKED LISTS";
+    return 0;
+}
